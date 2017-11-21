950e2e28b2b91ef2442bcd479170e095b280e55f
Unknown <natanael.ahac@outlook.com>
2017-11-21 14:57:05 +0000

    Zones logic and struct

--- alarm_system_project/alarm_system_project.ino
+++ alarm_system_project/alarm_system_project.ino
@@ -18,8 +18,31 @@
 #define D4 5
 #define D5 4
 #define D6 3
-#define D7 2 
+#define D7 2
 
+//Zone types
+#define ENTRY_EXIT 1
+#define ANALOG 2
+#define DIGITAL 3
+#define CONTINUOUS 4
+
+typedef struct zone {
+  int pin;
+  int type;
+
+  //ENTRY_EXIT PARAMETERS
+  int password;
+  int wait_time;
+
+  //ANALOG PARAMETERS
+  int analog_threshold;
+
+  //DIGITAL PARAMETERS
+  bool active_high_low;
+
+  //CONTINUOUS MONITORING PARAMETERS
+  bool high_to_low;
+};
 
 // initialize the library by associating any needed LCD interface pin
 // with the arduino pin number it is connected to
@@ -30,6 +53,8 @@ LiquidCrystal lcd(RS, EN, D4, D5, D6, D7);
 IRrecv irrecv(IR_RCV);
 decode_results results;
 
+zone zones[4];
+
 int curr_menu_index = 0;
 int menuLength = 5;
 char menu[5][16] = {"Set time", "Alarm Zone 1", "Alarm Zone 2", "Alarm Zone 3", "Alarm Zone 4"};
@@ -37,7 +62,7 @@ char menu[5][16] = {"Set time", "Alarm Zone 1", "Alarm Zone 2", "Alarm Zone 3", 
 
 volatile int count = 0;
 
-void setupIR(){
+void setupIR() {
   // In case the interrupt driver crashes on setup, give a clue
   // to the user what's going on.
   Serial.println("Enabling IRin");
@@ -45,9 +70,9 @@ void setupIR(){
   Serial.println("Enabled IRin");
 }
 
-void setupLCD(){
+void setupLCD() {
   //LCD contrast
-  analogWrite(CONTRAST_PIN,CONTRAST_VALUE); 
+  analogWrite(CONTRAST_PIN, CONTRAST_VALUE);
   // set up the LCD's number of columns and rows:
   lcd.begin(16, 2);
   // Print a message to the LCD.
@@ -57,28 +82,33 @@ void setupLCD(){
   
 }
 
-void setupTimer(){
-   TCCR1A = 0;
-   TCCR1B = 0;
-   OCR1A = 15625; //Set the count corresponding to 1 second
-   TCCR1B |= (1 << WGM12); //Turn on CTC mode
-   TCCR1B |= (1 << CS10);
-   TCCR1B |= (1 << CS12); //Prescale at 1024
-   TIMSK1 |= (1<< OCIE1A); //Enable CTC interrupt
+void setupTimer() {
+  TCCR1A = 0;
+  TCCR1B = 0;
+  OCR1A = 15625; //Set the count corresponding to 1 second
+  TCCR1B |= (1 << WGM12); //Turn on CTC mode
+  TCCR1B |= (1 << CS10);
+  TCCR1B |= (1 << CS12); //Prescale at 1024
+  TIMSK1 |= (1 << OCIE1A); //Enable CTC interrupt
 }
 
-void test(){
-  Serial.println("test interrupt");  
+void setZones() {
+  zones[0] = { .pin = 10, .type = ANALOG };
+  zones[1] = { .pin = 8, .type = DIGITAL };
+  zones[2] = { .pin = 10, .type = ENTRY_EXIT };
+  zones[3] = { .pin = 10, .type = CONTINUOUS };
 }
 
 void setup() {
   Serial.begin(9600);
   pinMode(BUTTON_PIN, INPUT);
+  pinMode(8, INPUT);
   cli(); //Disable global interrupts
   setupTimer();
   sei(); //Enable Global Interrupts
   setupLCD();
   setupIR();
+  setZones();
   //pinMode(BUZZER,OUTPUT);
 }
 
@@ -86,11 +116,11 @@ void setup() {
 
 void loop() {
   decodeIR();
+  checkForAlarm();
   //tone(BUZZER,1000);
 }
 
-
-ISR (TIMER1_COMPA_vect){
+ISR (TIMER1_COMPA_vect) {
   lcd.setCursor(0, 1);
   /*
   lcd.print(count);
@@ -99,16 +129,11 @@ ISR (TIMER1_COMPA_vect){
   count++;
 }
 
-ISR (PCINT1_vect) // handle pin change interrupt for A0 to A5 here
-{
-   test();
-}  
-
- boolean debounceRead(byte input){
-   if(digitalRead(input)){
-      delay(DEBOUNCE_TIME);
-      return digitalRead(input);
-   }
-   return LOW;
+boolean debounceRead(byte input) {
+  if (digitalRead(input)) {
+    delay(DEBOUNCE_TIME);
+    return digitalRead(input);
+  }
+  return LOW;
 }
 
---
+++ alarm_system_project/zone_logic.ino
@@ -0,0 +1,40 @@
+void checkForAlarm() {
+  for (int i = 0; i < sizeof(zones); i++) {
+    switch (zones[i].type) {
+      case ENTRY_EXIT:
+        //Serial.println("CHECKING ENTRY_EXIT");
+        checkEntryExit(zones[i], debounceRead(digitalRead(zones[i].pin)));
+        break;
+      case ANALOG:
+        //Serial.println("CHECKING ANALOG");
+        checkAnalog(zones[i], analogRead(zones[i].pin));
+        break;
+      case DIGITAL:
+        //Serial.println("CHECKING DIGITAL");
+        checkDigital(zones[i], debounceRead(zones[i].pin));
+        break;
+      case CONTINUOUS:
+        //Serial.println("CHECKING CONTINUOUS");
+        checkContinuous(zones[i], debounceRead(zones[i].pin));
+        break;
+    }
+  }
+}
+
+void checkAnalog(zone zone, int value) {
+  
+}
+
+void checkEntryExit(zone zone, bool value) {
+  if(!value){
+    Serial.println("Entry/Exit ALARM!");
+   }
+}
+
+void checkDigital(zone zone, int value) {
+    
+}
+
+void checkContinuous(zone zone, bool value) {
+
+}
